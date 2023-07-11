467cd8fc5de63fd13af9d363a476faa99d8d1d77
Anantha Ramakrishnan Subramanian <anantharamakrishnan19@gmail.com>
2023-07-11 10:06:48 +0530

    Anantha - Completed assignment 2.1 Softare-2.1-js-variables

---
+++ Anantha/2.1 Softare-2.1-js-variables/script.js
@@ -0,0 +1,32 @@
+let studentFirstName = "Brandon";
+let studentLastName = "N";
+let grade1 = 53;
+let grade2 = 96;
+let grade3 = 65;
+let grade4 = 74;
+let passingGrade = 80;
+//1.Create a variable called average that computes for the average from grade1 to grade4
+const avg = (grade1 + grade2 + grade3 + grade4)/4;
+console.log("Average of Grades:",avg);
+
+//2. Change the value of studentLastName to "Newman" 
+console.log("Student Last Name, before re-assigning:", studentLastName);
+studentLastName = "Newman";
+console.log("Student Last Name, after re-assigning:", studentLastName);
+
+//3. Print in console the following statement: "<value of studentFirstName> <value of studentLastName> has an average grade of <value of average>"
+//ex. Jane Doe has an average grade of 100.
+console.log(`${studentFirstName} ${studentLastName} has an average grade of ${avg}`);
+//Alternatively,
+console.log(studentFirstName + " " +  studentLastName + " has an average grade of " + avg);
+
+
+//4. Write code that would answer this question: Did the student pass?
+if(avg >= passingGrade)
+{
+    console.log(`${studentFirstName} ${studentLastName} passed with average grade of ${avg}`);
+}
+else
+{
+    console.log(`ohh!! ${studentFirstName} ${studentLastName}, you couldn't secure average grade ${passingGrade} required to pass. Better luck next time!!`);
+}
